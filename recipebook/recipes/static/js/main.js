96b5a7329d68c6e9123900274056981aae68d0cc
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-08 20:50:26 +0300

    feat: change title with select done

--- recipebook/recipes/static/js/main.js
+++ recipebook/recipes/static/js/main.js
@@ -3,6 +3,7 @@ const recipeByCatContainer = document.querySelector('.categories__recipies');
 const categorySelect = document.querySelector('#categorySelect');
 const newest = document.querySelector('.newest');
 
+//get recipes, filter by CategoryID, call display func + display last recipes
 async function getRecipesList(categoryId) {
     try {
         const response = await fetch('/api/recipes/');
@@ -24,29 +25,61 @@ async function getRecipesList(categoryId) {
 
 }
 
+//get info from API categories
 async function getCategoryList() {
+    
     try {
         const response = await fetch('/api/categories/');
         if (response.ok) {
             const getData = await response.json();
-            //console.log(getData);
+            //console.log(getData); // array of objects: ID + category name
+            
+            //for display list of category's names in the middle of page
             getData.forEach(async (item) => {
+                //go to show func
                 displayCategories(item, item['id']);
-
             });
-            // Call getRecipesList with the first category ID
-            //getRecipesList(getData[9].id);
 
+            // initially category - Pork
+            const initialCategoryId = getData[8].id;
+            displayRecipesByCategory(initialCategoryId);
+
+            //for select on the top
             getData.forEach(category => {
                 const option = document.createElement('option');
-                option.value = category.id;
-                option.textContent = category.name;
+                option.value = category['id'];
+                option.textContent = category['name'];
                 categorySelect.appendChild(option);
-              });
-        
-              // Display recipes for the initially selected category
-              const initialCategoryId = getData[8].id;
-              await displayRecipesByCategory(initialCategoryId);
+            });
+
+            // for category selection event change to get value of option
+            categorySelect.addEventListener('change', () => {
+                const selectedIndex = categorySelect.selectedIndex;
+                const selectedCategoryId = categorySelect.options[selectedIndex].value;
+
+                newest.textContent = '';
+
+                const title = document.querySelector('.categories__title');
+                const subtitle = document.querySelector('.categories__subtitle');
+                subtitle.textContent = '';
+
+                // console.log('Selected Category ID:', selectedCategoryId);
+                // console.log('Categories Data:', getData);
+
+                //category.id - toString because i need to compare with the same types
+                const selectedCategory = getData.find(category => category.id.toString() === selectedCategoryId);
+
+                if (selectedCategory) {
+                    //console.log('Selected Category:', selectedCategory);
+                    title.textContent = selectedCategory.name;
+                } else {
+                    //console.log('Selected Category not found.');
+                    title.textContent = '';
+                }
+
+                displayRecipesByCategory(selectedCategoryId);
+            });
+
         } else {
             throw new Error('Something wrong with fetch');
         }
@@ -58,6 +91,8 @@ async function getCategoryList() {
 }
 
 
+
+//when was click event on a category element - fetch the recipes for that category
 function handleCategoryClick(categoryId) {
     return async function (event) {
         event.preventDefault();
@@ -65,9 +100,8 @@ function handleCategoryClick(categoryId) {
             const response = await fetch(`/api/categories/${categoryId}/recipes/`);
             if (response.ok) {
                 const recipes = await response.json();
-
+                //console.log(recipes); // arr of objects filtered by category what was chosen
                 recipes.forEach(async recipe => {
-
                     displayRecepiesByCat(recipe);
                 });
             } else {
@@ -79,9 +113,10 @@ function handleCategoryClick(categoryId) {
     };
 }
 
-
+//display in DOM recipes after click on the link in list of category 
 function displayRecepiesByCat(recipe) {
     //console.log(recipe);
+
 
     const col = document.createElement('div');
     col.classList.add('col');
@@ -93,16 +128,15 @@ function displayRecepiesByCat(recipe) {
 }
 
 
-// Function to display recipes based on the selected category
+// display recipes based on the selected category from select on the top
 async function displayRecipesByCategory(categoryId) {
     try {
       const response = await fetch(`/api/categories/${categoryId}/recipes/`);
       if (response.ok) {
         const recipes = await response.json();
   
-        // Clear existing recipes from the container
-        ;
-        recipeByCatContainer.innerHTML = '';
+        // Clear container
+        recipeByCatContainer.textContent = '';
   
         recipes.forEach(recipe => {
           const card = createCard(recipe);
@@ -116,28 +150,26 @@ async function displayRecipesByCategory(categoryId) {
     }
   }
 
-// Event listener for category selection
-categorySelect.addEventListener('change', () => {
-    const selectedCategoryId = categorySelect.value;
-    newest.textContent = '';
-    displayRecipesByCategory(selectedCategoryId);
-});
 
-
+//show list of categories
 function displayCategories(obj, categoryId) {
     const categoryContainer = document.querySelector('.categories__list');
     const categoryUl = categoryContainer.querySelector('.categories__block');
 
-
+    //create link
     const li = document.createElement('li');
     const a = document.createElement('a');
     const aText = document.createTextNode(obj['name']);
     a.href = '#';
 
     a.appendChild(aText);
+
+    //prevent refresh page, add class to each link, 
     a.addEventListener('click', function (event) {
         event.preventDefault();
 
+        const title = document.querySelector('.categories__title');
+        title.textContent = '';
         // Remove the "active" class from all category links
         const allCategoryLinks = categoryUl.querySelectorAll('li a');
         allCategoryLinks.forEach(link => link.classList.remove('active'));
@@ -145,6 +177,7 @@ function displayCategories(obj, categoryId) {
         // Add the "active" class to the clicked category link
         a.classList.add('active');
 
+        //immediately call the returned function with event because we return it from handle
         handleCategoryClick(categoryId)(event);
         recipeByCatContainer.textContent = '';
     });
@@ -174,7 +207,6 @@ function createCard(obj) {
     const instrText = document.createTextNode('Step by step guide...');
 
 
-
     const img = document.createElement('img');
     img.src = obj['img'];
 
@@ -191,9 +223,11 @@ function createCard(obj) {
     card.appendChild(cardFooter);
 
 
+    //for popup window after click on card
     card.addEventListener(('click'), (e) => {
         e.preventDefault();
 
+        //got to show ingr + give they two params ingredients + instructions
         showIngredientsPopup(obj['instructions'], obj['ingredients']);
     })
 
@@ -202,40 +236,8 @@ function createCard(obj) {
 }
 
 
-function displayRecipes(obj) {
-    // console.log(obj);
-    const cardContainer = document.querySelector('.catalog');
-
-    const col = document.createElement('div');
-    col.classList.add('col');
-
-    const card = createCard(obj);
-
-    col.appendChild(card);
-    cardContainer.appendChild(col);
-}
-
-
-function displayLastRecipes(recipes) {
-
-    const lastRecipesContainer = document.querySelector('.lastRecipes');
-
-    // Clear any existing content in the container
-    lastRecipesContainer.textContent = '';
-
-    // Get the last four recipes
-    const lastFourRecipes = recipes.slice(-4);
-
-    lastFourRecipes.forEach(recipe => {
-
-        const card = createCard(recipe);
-        lastRecipesContainer.appendChild(card);
-    });
-}
-
-
 function showIngredientsPopup(instructions, ingredients) {
-    console.log(ingredients);
+    //console.log(ingredients);
     // Create and display the popup window with ingredients
     const popup = document.createElement('div');
     popup.classList.add('popup');
@@ -273,6 +275,7 @@ function showIngredientsPopup(instructions, ingredients) {
 
     document.body.appendChild(popup);
 
+    //comparing if we click not on the content window (background) - remove popup
     popup.addEventListener('click', function (event) {
         if (event.target === popup) {
             document.body.removeChild(popup);
@@ -280,12 +283,43 @@ function showIngredientsPopup(instructions, ingredients) {
     });
 
 
-    // Add event listener to close the popup
+    //close the popup on button
     closeBtn.addEventListener('click', function () {
         document.body.removeChild(popup);
     });
 }
 
+
+function displayRecipes(obj) {
+    // console.log(obj);
+    const cardContainer = document.querySelector('.catalog');
+
+    const col = document.createElement('div');
+    col.classList.add('col');
+
+    const card = createCard(obj);
+
+    col.appendChild(card);
+    cardContainer.appendChild(col);
+}
+
+
+function displayLastRecipes(recipes) {
+
+    const lastRecipesContainer = document.querySelector('.lastRecipes');
+
+    // Clear container
+    lastRecipesContainer.textContent = '';
+
+    // last four recipes from DB
+    const lastFourRecipes = recipes.slice(-4);
+
+    lastFourRecipes.forEach(recipe => {
+
+        const card = createCard(recipe);
+        lastRecipesContainer.appendChild(card);
+    });
+}
 
 //recipe CRUD
 async function createRecipe(recipeData) {
@@ -466,3 +500,5 @@ async function deleteCategory(ingredientId) {
 
 getCategoryList()
 getRecipesList()
+
+
